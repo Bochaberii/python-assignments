e2aa7655e9e1f448ed3e35a70290264a27af2d76
Sandra Biaki Keya <108887344+Bochaberii@users.noreply.github.com>
2025-08-05 22:35:43 +0300

    Final commit

---
+++ calc.py
@@ -0,0 +1,23 @@
+print("We will build a simple calculator!")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number:"))
+
+operation = input("Enter operation ; +, - , * , /")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+
+else:
+    result = "Invalid operation!"
+
+print(f"The result is: {result}")
